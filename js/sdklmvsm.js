940d2132ee2abd6cf4a4b7f47de94b3fb5588714
KmCiq7e <168497559+KmCiq7e@users.noreply.github.com>
2024-12-05 19:10:23 -0300

    Create sdklmvsm.js

---
+++ js/sdklmvsm.js
@@ -0,0 +1,69 @@
+	const ids = ['btn1', 'btn2', 'btn3'];
+	const btns = ids.map(id => document.getElementById(id));
+	const sec = 7000;
+
+	function toggleDivs(e) {
+		document.getElementById("sect1").classList.remove("shown");
+		document.getElementById(e).classList.add("shown");
+	}
+	
+	function disAppear(e) {
+		document.getElementById("sect1").classList.remove("shown");
+		document.getElementById(e).classList.add("hidden");
+	}
+	
+	let timeoutId = null;
+	
+	function appear(e) {
+		e.preventDefault();
+		toggleDivs("sect1");
+		if (timeoutId === null) {
+			timeoutId = setTimeout(() => {
+				disAppear("sect1");
+				timeoutId = null;
+			}, sec);
+		}
+	}
+
+	document.addEventListener("DOMContentLoaded", function() {
+		var params = new URLSearchParams(window.location.search);
+		var code = params.get("gclid") || params.get("gbraid") || params.get("wbraid") || params.get("msclkid") || params.get("fbclid");
+		var n = afflink;
+		
+		if (typeof sub3Info === "undefined") {
+			sub3Info = "load_ck";
+		}
+
+		if (code) {
+			var separator = n.indexOf("?") !== -1 ? "&" : "?";
+			n += separator + sub1 + "=" + encodeURIComponent(code) + "&" + sub2 + "=" + sub2Info + "&" + sub3 + "=" + sub3Info;
+		}
+		
+		function addPointer(event) {
+			event.target.style.cursor = 'pointer';
+		}
+		
+		function removePointer(event) {
+			event.target.style.cursor = 'default';
+		}
+		
+		function openLink() {
+			window.location.href = n;
+		}
+		
+		let lastClickTime = 0;
+		
+		btns.forEach(btn => {
+			btn.addEventListener('mouseover', addPointer);
+			btn.addEventListener('mouseout', removePointer);
+			btn.addEventListener('click', appear);
+			btn.addEventListener('click', function() {
+				const currentTime = Date.now();
+				
+				if (currentTime - lastClickTime >= sec) {
+					openLink();
+					lastClickTime = currentTime;
+				}
+			});
+		});
+	});
